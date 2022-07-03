2f57c0679b4ae3908b14a03ff40a4940da7f4f69
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-03 15:47:15 -0300

    feat: loading accounts from file and balance method

--- wallet-server/src/main.py
+++ wallet-server/src/main.py
@@ -4,10 +4,24 @@ import stubs.wallet_pb2 as wallet_pb2
 import stubs.wallet_pb2_grpc as wallet_pb2_grpc
 
 class Wallet:
-  def Balance(identifier):
-    print(identifier)
+  def __init__(self):
+    self.accounts = {}
+    self.__load_accounts()
+
+  def __load_accounts(self):
+    file = open("contas.txt")
+    for line in file:
+      data = line.split(" ")
+      self.accounts[data[0]] = int(data[1])
+
+  def Balance(self, identifier):
+    if not identifier in self.accounts:
+      return {
+        "balance": -1
+      }
+    
     return {
-      "balance": -1
+      "balance": self.accounts[identifier]
     }
 
 server = GRPCServer()

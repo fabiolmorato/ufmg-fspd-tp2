056ee429c8aab1a94c5eff49bb01e287f14d5617
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-03 15:34:25 -0300

    refactor: wallet client using GRPCClient abstraction

--- wallet-client/src/main.py
+++ wallet-client/src/main.py
@@ -1,12 +1,8 @@
-import grpc
+from common.GRPCClient import GRPCClient
+
 import stubs.wallet_pb2 as wallet_pb2
 import stubs.wallet_pb2_grpc as wallet_pb2_grpc
 
-def run():
-  with grpc.insecure_channel('localhost:5505') as channel:
-    stub = wallet_pb2_grpc.WalletStub(channel)
-    response = stub.Balance(wallet_pb2.BalanceParams(identifier="ansbdhas"))
-    print("Response: " + str(response.balance))
-
-if __name__ == "__main__":
-  run()
+client = GRPCClient("0.0.0.0", 5505)
+Wallet = client.get_provider("Wallet", wallet_pb2, wallet_pb2_grpc)
+print(Wallet.Balance(identifier="asbdfhsbh"))

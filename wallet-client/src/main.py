81a5c8dbc705bb45fc05ef757e66320fc4736893
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2022-07-03 16:12:09 -0300

    feat: using Stop method in wallet client

--- wallet-client/src/main.py
+++ wallet-client/src/main.py
@@ -5,4 +5,5 @@ import stubs.wallet_pb2_grpc as wallet_pb2_grpc
 
 client = GRPCClient("0.0.0.0", 5505)
 Wallet = client.get_provider("Wallet", wallet_pb2, wallet_pb2_grpc)
-print(Wallet.Balance(identifier="asbdfhsbh"))
+# print(Wallet.Balance(identifier="Zeh"))
+Wallet.Stop()
